177d03583bd1d486842668c564cfae33fc3989ec
yetiasg <yetiasg@gmail.com>
2021-07-28 19:59:52 +0200

    done UI and styles

--- gradientGenerator.js
+++ gradientGenerator.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const buttons = document.querySelectorAll('[data-direction]');
+
+const selectedDirection = 'top to right';
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(btn.dataset.direction);
+    buttons.forEach((btn) => {
+      btn.childNodes[1].children[0].removeAttribute(
+        'class',
+        'selectedDirection'
+      );
+    });
+    btn.childNodes[1].children[0].setAttribute('class', 'selectedDirection');
+  });
+});
