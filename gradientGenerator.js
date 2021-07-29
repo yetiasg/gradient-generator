f0cf726ae5b469746feb6aaeb8e9dcecf78b808f
yetiasg <yetiasg@gmail.com>
2021-07-29 12:47:11 +0200

    Gradien generator done

--- gradientGenerator.js
+++ gradientGenerator.js
@@ -1,18 +1,52 @@
 'use strict';
 
-const buttons = document.querySelectorAll('[data-direction]');
+const fetchColors = () => {
+  const color1 = document.getElementById('color1').value;
+  const color2 = document.getElementById('color2').value;
+  return { color1, color2 };
+};
 
-const selectedDirection = 'top to right';
+let direction = 'to right bottom';
+
+const buttons = document.querySelectorAll('[data-direction]');
 buttons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(btn.dataset.direction);
-    buttons.forEach((btn) => {
-      btn.childNodes[1].children[0].removeAttribute(
-        'class',
-        'selectedDirection'
-      );
+    direction = btn.dataset.direction;
+    //change arrow color
+    buttons.forEach((button) => {
+      clearArrowColor(button);
     });
-    btn.childNodes[1].children[0].setAttribute('class', 'selectedDirection');
+    changeArrowColor(btn);
   });
+});
+
+const clearArrowColor = (btn) => {
+  btn.childNodes[1].children[0].removeAttribute('class', 'selectedDirection');
+};
+
+const changeArrowColor = (btn) => {
+  btn.childNodes[1].children[0].setAttribute('class', 'selectedDirection');
+};
+
+const generateGradient = (direction, { color1, color2 }) => {
+  return `linear-gradient(${direction}, ${color1}, ${color2})`;
+};
+
+const changeBackgroundGradient = (gradientValue) => {
+  const gradientSection = document.querySelector('.gradient');
+  gradientSection.style.backgroundImage = gradientValue;
+};
+
+const updateBackgroundValueDisplay = (gradientValue) => {
+  const outputGradient = document.querySelector('.outputGradient p');
+  outputGradient.innerText = `background-image: ${gradientValue};`;
+};
+
+const submitBtn = document.querySelector('[type=submit]');
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const gradientValue = generateGradient(direction, fetchColors());
+  changeBackgroundGradient(gradientValue);
+  updateBackgroundValueDisplay(gradientValue);
 });
